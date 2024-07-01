8f891fa31c755614bb011db030b802cd238f710a
kimkun07 <kimkun07@naver.com>
2024-07-01 14:18:25 +0900

    echo server

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -6,3 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/echo")
+def echo():
+    message = request.args.get(key="message")
+    if message is None:
+        return f"Test echo with /echo?message={escape("<message>")}"
+    else:
+        return f"echo: {escape(message)}"
