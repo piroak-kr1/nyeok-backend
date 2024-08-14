21def23808d92f36fe9ea746a5efe419abec913c
kimkun07 <kimkun07@naver.com>
2024-08-14 13:33:43 +0900

    [Routes] 프론트에서 origin/destination 입력 받기 성공

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Response
+from pydantic import BaseModel
+from pydantic_extra_types.coordinate import Coordinate
 from .network import routes_api
 
 app = FastAPI()
@@ -21,4 +23,19 @@ async def echo(message: str | None = None) -> str:
 @app.get("/compute_routes_sample")
 async def compute_routes_sample() -> Response:
     resultJson: str = await routes_api.sample_compute_routes()
+    return Response(content=resultJson, media_type="application/json")
+
+
+class RouteRequest(BaseModel):
+    origin: Coordinate
+    destination: Coordinate
+
+
+@app.post("/compute_routes")
+async def compute_routes(request: RouteRequest) -> Response:
+    origin: Coordinate = request.origin
+    destination: Coordinate = request.destination
+    resultJson: str = await routes_api.compute_routes(
+        origin=origin, destination=destination
+    )
     return Response(content=resultJson, media_type="application/json")
--- app/network/routes_api.py
+++ app/network/routes_api.py
@@ -1,9 +1,17 @@
 from google.maps import routing_v2
 from google.type.latlng_pb2 import LatLng
 from google.protobuf.json_format import MessageToJson
+from pydantic_extra_types.coordinate import Coordinate
 
 
 async def sample_compute_routes() -> str:
+    return await compute_routes(
+        origin=Coordinate(latitude=36.0192418, longitude=129.3242741),
+        destination=Coordinate(latitude=36.0214277, longitude=129.3370694),
+    )
+
+
+async def compute_routes(origin: Coordinate, destination: Coordinate) -> str:
     # Create a client
     client = routing_v2.RoutesAsyncClient()
 
@@ -11,12 +19,14 @@ async def sample_compute_routes() -> str:
     request = routing_v2.ComputeRoutesRequest(
         origin=routing_v2.Waypoint(
             location=routing_v2.Location(
-                lat_lng=LatLng(latitude=36.0192418, longitude=129.3242741)
+                lat_lng=LatLng(latitude=origin.latitude, longitude=origin.longitude)
             )
         ),
         destination=routing_v2.Waypoint(
             location=routing_v2.Location(
-                lat_lng=LatLng(latitude=36.0214277, longitude=129.3370694)
+                lat_lng=LatLng(
+                    latitude=destination.latitude, longitude=destination.longitude
+                )
             )
         ),
         travel_mode=routing_v2.RouteTravelMode.TRANSIT,
