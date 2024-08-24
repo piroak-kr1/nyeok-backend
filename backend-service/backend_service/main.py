3c8ef9e26cb0b8697f25d3e856f4bc5a3157e1fa
kimkun07 <kimkun07@naver.com>
2024-08-24 13:09:49 +0900

    places_sample: DB의 첫 번째 place를 반환한다

--- backend-service/backend_service/main.py
+++ backend-service/backend_service/main.py
@@ -1,11 +1,27 @@
 import logging
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Depends
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from pydantic_extra_types.coordinate import Coordinate
+from sqlalchemy import func
+from sqlalchemy.orm import Session
+
+
 from .outbound import routes_api
+from .models import Place
+from database_core import db
+from database_core.tables import Place as DBPlace
 
 
 app = FastAPI()
+
+db.setup(
+    username="superuser",
+    password="wrongpassword",
+    hostname="localhost",
+    port=5432,
+    databasename="database",
+)
 
 
 @app.get("/")
@@ -22,10 +38,24 @@ async def echo(message: str | None = None) -> str:
         return f"{message}"
 
 
+@app.get("/places_sample", response_model=Place)
+def places_sample(session: Session = Depends(db.get_session_yield)):
+    single_record: tuple[DBPlace, float, float] | None = session.query(
+        DBPlace,
+        func.ST_X(DBPlace.coordinate),
+        func.ST_Y(DBPlace.coordinate),
+    ).first()  # type: ignore
+    if single_record is None:
+        raise ValueError("No records found")
+
+    dbPlace, longitude, latitude = single_record
+    return Place.from_sqlalchemy_model(dbPlace, longitude, latitude)
+
+
 @app.get("/compute_routes_sample")
-async def compute_routes_sample() -> Response:
+async def compute_routes_sample() -> JSONResponse:
     resultJson: str = await routes_api.sample_compute_routes()
-    return Response(content=resultJson, media_type="application/json")
+    return JSONResponse(content=resultJson, media_type="application/json")
 
 
 class RouteRequest(BaseModel):
@@ -34,24 +64,24 @@ class RouteRequest(BaseModel):
 
 
 @app.post("/compute_routes")
-async def compute_routes(request: RouteRequest) -> Response:
+async def compute_routes(request: RouteRequest) -> JSONResponse:
     origin: Coordinate = request.origin
     destination: Coordinate = request.destination
     resultJson: str = await routes_api.compute_routes(
         origin=origin, destination=destination
     )
-    return Response(content=resultJson, media_type="application/json")
-
-
-class EndpointFilter(logging.Filter):
-    def filter(self, record: logging.LogRecord) -> bool:
-        return record.getMessage().find('"GET /readiness HTTP/1.1" 200') == -1
-
-
-# Filter out /readiness
-logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
+    return JSONResponse(content=resultJson, media_type="application/json")
 
 
 @app.get("/readiness")
 async def readiness():
     return {"status": "ok"}
+
+
+# Filter out /readiness
+class EndpointFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.getMessage().find('"GET /readiness HTTP/1.1" 200') == -1
+
+
+logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
---
+++ backend-service/backend_service/models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, HttpUrl
+from pydantic_core import Url
+from pydantic_extra_types.coordinate import Coordinate, Longitude, Latitude
+
+from database_core import tables
+
+
+class Place(BaseModel):
+    contentid: int
+    title: str
+    coordinate: Coordinate
+    firstimage2: HttpUrl
+
+    class Config:
+        from_attributes = True  # SQLALchemy model to Pydantic model
+
+    @staticmethod
+    def from_sqlalchemy_model(dbPlace: tables.Place, longitude: float, latitude: float):
+        return Place(
+            contentid=dbPlace.contentid,
+            title=dbPlace.title,
+            coordinate=Coordinate(
+                longitude=Longitude(longitude), latitude=Latitude(latitude)
+            ),
+            firstimage2=Url(dbPlace.firstimage2),
+        )
