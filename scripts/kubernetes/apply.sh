4439f15379c3c3d0477e325031de17cc68356b83
kimkun07 <kimkun07@naver.com>
2024-08-28 21:14:15 +0900

    secret: script에서 생성하는 대신 secret generator 이용

--- scripts/kubernetes/apply.sh
+++ scripts/kubernetes/apply.sh
@@ -5,14 +5,18 @@ if [[ "$1" == "minikube" ]]; then
   scripts_dir=$(dirname "$0")
   start_minikube="$scripts_dir/start_minikube.sh"
   "$start_minikube"
-fi
-if [[ "$1" == "minikube" || "$1" == "gke" ]]; then
+  
   kubectl config use-context "$1"
-  # kubectl delete secret --all
-  # kubectl create secret generic cluster-secret \
-  #   --from-file=secret-backend=.k8s/.secret.backend \
-  #   --from-file=cloudflare-token=.k8s/.secret.cloudflare
   kubectl apply -k ./k8s/overlays/"$1"
+  
+  echo ""
+  echo "Minikube available at: http://localhost, http://localhost/api"
+elif [[ "$1" == "gke" ]]; then
+  kubectl config use-context "$1"
+  kubectl apply -k ./k8s/overlays/"$1"
+  
+  echo ""
+  echo "GKE availabe at: https://piroak.com"
 else
   echo "Usage: ./kapply.sh minikube|gke"
 fi
