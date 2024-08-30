9ac5b8f534977781f6f9bd228729dfa41c13a942
kimkun07 <kimkun07@naver.com>
2024-08-30 17:52:35 +0900

    0.5.0: pypi install과 local path install 모두 작동

---
+++ backend-service/README.md
@@ -0,0 +1,6 @@
+### DockerImage
+
+Environment variables:
+
+- ROOT_PATH: defaults to `/`
+- GCP_API_KEY
---
+++ scripts/publish/backend-service.sh
@@ -0,0 +1,28 @@
+# Argument: major, minor, patch
+
+# Change directory for subsequent poetry commands
+cd "./backend-service"
+
+prev_version=$(poetry version)
+
+if [ "$1" == "major" ]; then
+    echo "Bumping major version"
+    poetry version major
+elif [ "$1" == "minor" ]; then
+    echo "Bumping minor version"
+    poetry version minor
+elif [ "$1" == "patch" ]; then
+    echo "Bumping patch version"
+    poetry version patch
+elif [ "$1" == "again" ]; then
+    echo "Not changing version: build & publish again"
+else
+    echo "Invalid argument. Please use major, minor, or patch (again)"
+    exit 1
+fi
+
+# NOTE: You should have installed poetry-plugin-mono-repo-deps plugin
+poetry export --without-hashes --output requirements.txt
+
+docker build --tag kimkun07/nyeok-backend:$(poetry version --short) .
+docker push kimkun07/nyeok-backend:$(poetry version --short)
